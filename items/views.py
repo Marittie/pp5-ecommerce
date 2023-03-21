71f5cfc7927479df4b80d41de5a60e46398b072a
Marittie <mario.castaldo1992@gmail.com>
2023-03-21 14:31:49 +0000

    Added category filtering

--- items/views.py
+++ items/views.py
@@ -42,7 +42,7 @@ def all_items(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any searh criteria!")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('items'))
 
             queries = Q(name__icontains=query) | Q(
